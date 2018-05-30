0be4f06fde2f524446e2ade3afca5490a7e4148a
Weissinger <Keyton.Weissinger@corp.tcc.inet>
2018-05-30 10:16:45 -0400

    Updated through step 4.

--- engine.py
+++ engine.py
@@ -1,15 +1,43 @@
 import tdl
+from components.fighter import Fighter
+from death_functions import kill_monster, kill_player
+from entity import Entity, get_blocking_entities_at_location
+from game_states import GameStates
 from input_handlers import handle_keys
-from entity import Entity
-from render_functions import clear_all, render_all
+from map_utils import GameMap, make_map
+from render_functions import clear_all, render_all, RenderOrder
+import datetime
 
 def main():
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
 
-    player = Entity(int(screen_width /2), int(screen_height / 2), '@', (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', (255, 255, 0))
-    entities = [npc, player]
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    fov_algorithm = 'BASIC'
+    fov_light_walls = True
+    fov_radius = 10
+
+    max_monsters_per_room = 3
+
+    colors = {
+        'dark_wall': (0, 0, 100),
+        'dark_ground': (50, 50, 150),
+        'light_wall': (130, 110, 50),
+        'light_ground': (200, 150, 50),
+        'desaturated_green': (63, 127, 63),
+        'darker_green': (0, 127, 0),
+        'dark_red': (191, 0, 0)
+    }
+
+    fighter_component = Fighter(hp=30, defense=2, power=5)
+    player = Entity(0, 0, '@', (255, 255, 255), 'Player', \
+                    blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component)
+    entities = [player]
 
     player_x = int(screen_width / 2)
     player_y = int(screen_height / 2)
@@ -19,13 +47,24 @@ def main():
     root_console = tdl.init(screen_width, screen_height, title='Roguelike Tutorial Revised', \
                             renderer='GLSL')
     con = tdl.Console(screen_width, screen_height)
+    game_map = GameMap(map_width, map_height)
+    make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player, \
+             entities, max_monsters_per_room, colors)
+
+    fov_recompute = True
+    game_state = GameStates.PLAYERS_TURN
 
     while not tdl.event.is_window_closed():
-        render_all(con, entities, root_console, screen_width, screen_height)
+        if fov_recompute:
+            game_map.compute_fov(player.x, player.y, fov=fov_algorithm, radius=fov_radius, light_walls=fov_light_walls)
+
+            render_all(con, entities, player, game_map, fov_recompute, root_console, screen_width, screen_height,
+                       colors)
         tdl.flush()
 
         clear_all(con, entities)
 
+        fov_recompute = False
 
         for event in tdl.event.get():
             if event.type == 'KEYDOWN':
@@ -44,11 +83,22 @@ def main():
         fullscreen = action.get('fullscreen')
         screenshot = action.get('screenshot')
 
-        if move:
+        player_turn_results = []
+
+        if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
-            player.move(dx, dy)
+            destination_x = player.x + dx
+            destination_y = player.y + dy
+            if game_map.walkable[destination_x, destination_y]:
+                target = get_blocking_entities_at_location(entities, destination_x, destination_y)
+                if target:
+                    attack_results = player.fighter.attack(target)
+                    player_turn_results.extend(attack_results)
+                else:
+                    player.move(dx, dy)
+                    fov_recompute = True
+                game_state = GameStates.ENEMY_TURN
         if screenshot:
-            print('taking screenshot?')
             tdl.screenshot()
 
         if exit:
@@ -56,6 +106,40 @@ def main():
 
         if fullscreen:
             tdl.set_fullscreen(not tdl.get_fullscreen())
+
+        for player_turn_result in player_turn_results:
+            message = player_turn_result.get('message')
+            dead_entity = player_turn_result.get('dead')
+            if message:
+                print(message)
+            if dead_entity:
+                if dead_entity == player:
+                    message, game_state = kill_player(dead_entity, colors)
+                else:
+                    message = kill_monster(dead_entity, colors)
+                print(message)
+
+        if game_state == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity.ai:
+                    enemy_turn_results = entity.ai.take_turn(player, game_map, entities)
+                    for enemy_turn_result in enemy_turn_results:
+                        message = enemy_turn_result.get('message')
+                        dead_entity = enemy_turn_result.get('dead')
+                        if message:
+                            print(message)
+                        if dead_entity:
+                            if dead_entity == player:
+                                message, game_state = kill_player(dead_entity, colors)
+                            else:
+                                message = kill_monster(dead_entity, colors)
+                            print(message)
+                            if game_state == GameStates.PLAYER_DEAD:
+                                break
+                if game_state == GameStates.PLAYER_DEAD:
+                    break
+            else:
+                game_state = GameStates.PLAYERS_TURN
 
 if __name__ == '__main__':
     main()
--- render_functions.py
+++ render_functions.py
@@ -1,8 +1,38 @@
-def render_all(con, entities, root_console, screen_width, screen_height):
-    # Draw all entities on the screen.
-    for entity in entities:
-        draw_entity(con, entity)
+from enum import Enum
 
+
+class RenderOrder(Enum):
+    CORPSE = 1
+    ITEM = 2
+    ACTOR = 3
+
+
+def render_all(con, entities, player, game_map, fov_recompute, root_console, \
+               screen_width, screen_height, colors):
+    # Draw all the tiles in the game map
+    if fov_recompute:
+
+        for x, y in game_map:
+            wall = not game_map.transparent[x, y]
+
+            if game_map.fov[x, y]:
+                if wall:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('light_wall'))
+                else:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('light_ground'))
+                game_map.explored[x][y] = True
+            elif game_map.explored[x][y]:
+                if wall:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('dark_wall'))
+                else:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('dark_ground'))
+
+    # Draw all entities on the screen.
+    entities_in_render_order = sorted(entities, key=lambda x: x.render_order.value)
+
+    for entity in entities_in_render_order:
+        draw_entity(con, entity, game_map.fov)
+    con.draw_str(1, screen_height - 2, 'HP: {0:02}/{1:02}''.format(player.fighter.hp, player.fighter.max_hp')
     root_console.blit(con, 0, 0, screen_width, screen_height, 0, 0)
 
 
@@ -11,8 +41,9 @@ def clear_all(con, entities):
         clear_entity(con, entity)
 
 
-def draw_entity(con, entity):
-    con.draw_char(entity.x, entity.y, entity.char, entity.color, bg = None)
+def draw_entity(con, entity, fov):
+    if fov[entity.x, entity.y]:
+        con.draw_char(entity.x, entity.y, entity.char, entity.color, bg = None)
 
 
 def clear_entity(con, entity):
